12ed2d4b2c4e0ebf2840f555571ad8f3f2d3f36c
Madalin Popa <contact@madalinpopa.com>
2024-10-10 20:15:16 +0300

    🧹 Simplify client test by removing redundant header checks

--- client_test.go
+++ client_test.go
@@ -1,33 +1,11 @@
 package webs
 
-import (
-	"net/http"
-	"testing"
-)
+import "testing"
 
-func TestClient_requestHeaders(t *testing.T) {
-	client := Client{}
-	commonHeaders := make(http.Header)
-	commonHeaders.Set("Content-Type", "application/json")
-	commonHeaders.Set("User-Agent", "go-client-client")
+func TestClientBuilder_Build(t *testing.T) {
 
-	client.builder.SetHeaders(commonHeaders)
-
-	requestHeaders := make(http.Header)
-	requestHeaders.Set("X-Request-ID", "abc123")
-
-	finalHeaders := mergeHeaders(commonHeaders, requestHeaders)
-	if len(finalHeaders) != 3 {
-		t.Errorf("expected 3 headers, got %d", len(finalHeaders))
-	}
-	if finalHeaders.Get("Content-Type") != "application/json" {
-		t.Errorf("expected Content-Type to be application/json, got %s", finalHeaders.Get("Content-Type"))
-	}
-	if finalHeaders.Get("User-Agent") != "go-client-client" {
-		t.Errorf("expected User-Agent to be go-client-client, got %s", finalHeaders.Get("User-Agent"))
-	}
-
-	if finalHeaders.Get("X-Request-ID") != "abc123" {
-		t.Errorf("expected X-Request-ID to be abc123, got %s", finalHeaders.Get("X-Request-ID"))
+	client := NewClientBuilder().Build()
+	if client == nil {
+		t.Errorf("expected client, got nil")
 	}
 }
