1376930b1a5bcd5d095c683fc19a7219019480c3
Madalin Popa <contact@madalinpopa.com>
2024-10-10 20:15:11 +0300

    🛠️ Remove custom HTTP client timeout and transport settings

--- client.go
+++ client.go
@@ -4,29 +4,13 @@ import (
 	"bytes"
 	"errors"
 	"io"
-	"net"
 	"net/http"
-	"sync"
-	"time"
-)
-
-const (
-
-	// defaultResponseTimeout is the default duration to wait for a response before timing out in HTTP client requests.
-	defaultResponseTimeout = 0
-
-	// defaultConnectionTimeout specifies the default timeout duration for establishing a connection to an HTTP server.
-	defaultConnectionTimeout = 0
-
-	// defaultMaxIdleConnsPerHost specifies the default maximum number of idle connections to keep per host in the HTTP client.
-	defaultMaxIdleConnsPerHost = 1
 )
 
 // Client represents a customizable HTTP client built with the help of ClientBuilder.
 type Client struct {
-	builder    *ClientBuilder
-	client     *http.Client
-	clientOnce sync.Once
+	builder *ClientBuilder
+	client  *http.Client
 }
 
 // ExecuteRequest sends an HTTP request with the specified method, URL, headers, and body, then returns the response.
@@ -45,8 +29,7 @@ func (c *Client) ExecuteRequest(method, url string, headers http.Header, body in
 
 	request.Header = allHeaders
 
-	client := c.getHttpClient()
-	response, err := client.Do(request)
+	response, err := c.client.Do(request)
 	if err != nil {
 		return nil, err
 	}
@@ -93,56 +76,4 @@ func (c *Client) Patch(url string, headers http.Header, body interface{}) (*Resp
 // Delete sends an HTTP DELETE request to the specified URL with optional headers and returns the response.
 func (c *Client) Delete(url string, headers http.Header) (*Response, error) {
 	return c.ExecuteRequest(http.MethodDelete, url, headers, nil)
-}
-
-// getHttpClient returns a singleton instance of http.Client configured with custom timeouts and transport settings.
-func (c *Client) getHttpClient() *http.Client {
-	if c.client != nil {
-		return c.client
-	}
-
-	c.clientOnce.Do(func() {
-		c.client = &http.Client{
-			Timeout: c.getConnectionTimeout(),
-			Transport: &http.Transport{
-				MaxIdleConnsPerHost:   c.getMaxIdleConnsPerHost(),
-				ResponseHeaderTimeout: c.getResponseTimeout(),
-				DialContext: (&net.Dialer{
-					Timeout: c.getConnectionTimeout(),
-				}).DialContext,
-			},
-		}
-	})
-
-	return c.client
-}
-
-// getResponseTimeout calculates and returns the appropriate response timeout duration for the HTTP client.
-func (c *Client) getResponseTimeout() time.Duration {
-	if c.builder.responseTimeout > 0 {
-		return c.builder.responseTimeout
-	}
-	if c.builder.disableTimeouts {
-		return 0
-	}
-	return defaultResponseTimeout
-}
-
-// getConnectionTimeout returns the configured connection timeout duration or the default value if not set.
-func (c *Client) getConnectionTimeout() time.Duration {
-	if c.builder.connectTimeout > 0 {
-		return c.builder.connectTimeout
-	}
-	if c.builder.disableTimeouts {
-		return 0
-	}
-	return defaultConnectionTimeout
-}
-
-// getMaxIdleConnsPerHost returns the maximum number of idle connections per host. If not set, defaults to 1.
-func (c *Client) getMaxIdleConnsPerHost() int {
-	if c.builder.maxIdleConnsPerHost > 0 {
-		return c.builder.maxIdleConnsPerHost
-	}
-	return defaultMaxIdleConnsPerHost
 }
