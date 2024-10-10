668e9a96351c986a0f9fb8f3bc9119259ba84f77
Madalin Popa <contact@madalinpopa.com>
2024-10-10 21:52:42 +0300

    📝 Rename test function for client build action

--- client_test.go
+++ client_test.go
@@ -2,10 +2,15 @@ package webs
 
 import "testing"
 
-func TestClientBuilder_Build(t *testing.T) {
+func TestClient_Build(t *testing.T) {
 
 	client := NewClientBuilder().Build()
 	if client == nil {
 		t.Errorf("expected client, got nil")
 	}
 }
+
+//func TestClient_Get(t *testing.T) {
+//	builder := NewClientBuilder()
+//
+//}
